f0df60a4fe4af8a0269ec248bd261f4d2c6c831f
Soby Chacko <schacko@pivotal.io>
2018-04-05 14:55:23 -0400

    build cleanup

--- jobs/springcloudstream/spring_cloud_stream.groovy
+++ jobs/springcloudstream/spring_cloud_stream.groovy
@@ -6,31 +6,17 @@ import javaposse.jobdsl.dsl.DslFactory
 
 DslFactory dsl = this
 
-boolean masterRelease = false
-String releaseType = "" // possible values are - milestone or ga
+//doMasterGAReleaseBuild(dsl)
+//doMasterMilestoneReleaseBuild(dsl)
 
-// Master builds (Elmhurst) for release (Excluding kinesis) - Uncomment this for release
+doMasterSnapshotBuild(dsl)
 
-//new SpringCloudStreamPhasedBuildMaker(dsl).build(['spring-cloud-stream-binder-kafka':'master',
-//                                                  'spring-cloud-stream-binder-rabbit':'master'], masterRelease, releaseType)
+doDitmarsSnapshotBuild(dsl)
 
-//Comment the following for the release
-new SpringCloudStreamPhasedBuildMaker(dsl).build(['spring-cloud-stream-binder-kafka':'master',
-                                                  'spring-cloud-stream-binder-rabbit':'master',
-                                                  'spring-cloud-stream-binder-aws-kinesis':'master'], masterRelease, releaseType)
+doChelseaSnapshotBuild(dsl)
 
-// Spring Cloud Stream Ditmars builds (1.3.x)
-new SpringCloudStreamPhasedBuildMaker(dsl).build("1.3.x", "Ditmars.x", "spring-cloud-stream-Ditmars-x-builds",
-                                                ['spring-cloud-stream-binder-kafka':'1.3.x',
-                                                'spring-cloud-stream-binder-rabbit':'1.3.x'], false, "", "Ditmars")
-// Spring Cloud Stream Chelsea builds (1.2.x)
-new SpringCloudStreamPhasedBuildMaker(dsl).build("1.2.x", "Chelsea.x", "spring-cloud-stream-Chelsea-x-builds",
-                                                ['spring-cloud-stream-binder-kafka':'1.2.x',
-                                                'spring-cloud-stream-binder-rabbit':'1.2.x'], false, "")
-// Spring Cloud Stream Brooklyn builds (1.1.x)
-new SpringCloudStreamPhasedBuildMaker(dsl).build("1.1.x", "Brooklyn.x", "spring-cloud-stream-Brooklyn-x-builds",
-                                                ['spring-cloud-stream-binder-kafka':'1.1.x',
-                                                 'spring-cloud-stream-binder-rabbit':'1.1.x'], false, "")
+doBrooklynSnapshotBuild(dsl)
+
 // 1.0.x builds
 new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream",
         "1.0.x", [KAFKA_TIMEOUT_MULTIPLIER: '60']).deploy(true, false, "clean deploy -Pfull,spring")
@@ -63,3 +49,43 @@ new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binde
 //new SpringCloudStreamBuildMarker(dsl, "spring-cloud", "spring-cloud-stream-binder-kafka",
 //        "0.11", [KAFKA_TIMEOUT_MULTIPLIER: '60']).deploy(true, false, "",
 //        null, null, null, false, true, "ga")
+
+
+void doMasterGAReleaseBuild(DslFactory dsl){
+    new SpringCloudStreamPhasedBuildMaker(dsl).build(['spring-cloud-stream-binder-kafka':'master',
+                                                  'spring-cloud-stream-binder-rabbit':'master'], true, "ga")
+
+}
+
+void doMasterMilestoneReleaseBuild(DslFactory dsl){
+    new SpringCloudStreamPhasedBuildMaker(dsl).build(['spring-cloud-stream-binder-kafka':'master',
+                                                      'spring-cloud-stream-binder-rabbit':'master'], true, "milestone")
+
+}
+
+void doMasterSnapshotBuild(DslFactory dsl){
+    new SpringCloudStreamPhasedBuildMaker(dsl).build(['spring-cloud-stream-binder-kafka':'master',
+                                                      'spring-cloud-stream-binder-rabbit':'master',
+                                                      'spring-cloud-stream-binder-aws-kinesis':'master'], false, "")
+}
+
+void doDitmarsSnapshotBuild(DslFactory dsl){
+    // Spring Cloud Stream Ditmars builds (1.3.x)
+    new SpringCloudStreamPhasedBuildMaker(dsl).build("1.3.x", "Ditmars.x", "spring-cloud-stream-Ditmars-x-builds",
+            ['spring-cloud-stream-binder-kafka':'1.3.x',
+             'spring-cloud-stream-binder-rabbit':'1.3.x'], false, "", "Ditmars")
+}
+
+void doChelseaSnapshotBuild(DslFactory dsl) {
+// Spring Cloud Stream Chelsea builds (1.2.x)
+    new SpringCloudStreamPhasedBuildMaker(dsl).build("1.2.x", "Chelsea.x", "spring-cloud-stream-Chelsea-x-builds",
+            ['spring-cloud-stream-binder-kafka' : '1.2.x',
+             'spring-cloud-stream-binder-rabbit': '1.2.x'], false, "")
+}
+
+void doBrooklynSnapshotBuild(DslFactory dsl) {
+// Spring Cloud Stream Brooklyn builds (1.1.x)
+    new SpringCloudStreamPhasedBuildMaker(dsl).build("1.1.x", "Brooklyn.x", "spring-cloud-stream-Brooklyn-x-builds",
+            ['spring-cloud-stream-binder-kafka' : '1.1.x',
+             'spring-cloud-stream-binder-rabbit': '1.1.x'], false, "")
+}
