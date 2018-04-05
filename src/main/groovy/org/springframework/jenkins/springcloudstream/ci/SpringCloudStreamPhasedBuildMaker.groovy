7c4b557d868f2734537b98c85c6c3cbb1cf2992a
Soby Chacko <schacko@pivotal.io>
2018-04-05 13:12:15 -0400

    cleaning up build

--- src/main/groovy/org/springframework/jenkins/springcloudstream/ci/SpringCloudStreamPhasedBuildMaker.groovy
+++ src/main/groovy/org/springframework/jenkins/springcloudstream/ci/SpringCloudStreamPhasedBuildMaker.groovy
@@ -8,11 +8,6 @@ import org.springframework.jenkins.springcloudstream.common.SpringCloudStreamJob
  */
 class SpringCloudStreamPhasedBuildMaker implements SpringCloudStreamJobs {
 
-    //public static final List<String> BINDER_PHASE_JOBS = ['spring-cloud-stream-binder-kafka', 'spring-cloud-stream-binder-rabbit',
-      //                                      'spring-cloud-stream-binder-aws-kinesis']
-
-   // public static final List<String> BINDER_PHASE_JOBS = ['spring-cloud-stream-binder-kafka', 'spring-cloud-stream-binder-rabbit']
-
     private final DslFactory dsl
 
     SpringCloudStreamPhasedBuildMaker(DslFactory dsl) {
