d3874a6aae3bc603e6d7166af3c357eab0e5c7fe
Soby Chacko <schacko@pivotal.io>
2018-11-05 16:13:14 -0500

    Update the cf credentials for acceptance tests.

--- src/main/groovy/org/springframework/jenkins/springcloudstream/ci/SpringCloudStreamBuildMarker.groovy
+++ src/main/groovy/org/springframework/jenkins/springcloudstream/ci/SpringCloudStreamBuildMarker.groovy
@@ -80,7 +80,7 @@ class SpringCloudStreamBuildMarker implements JdkConfig, TestPublisher,
                 }
                 if (ciPlanName.equals("spring-cloud-stream-cf-acceptance-tests")) {
                     credentialsBinding {
-                        usernamePassword('CF_E2E_TEST_SPRING_CLOUD_STREAM_USER', 'CF_E2E_TEST_SPRING_CLOUD_STREAM_PASSWORD', "needles-admin")
+                        usernamePassword('CF_E2E_TEST_SPRING_CLOUD_STREAM_USER', 'CF_E2E_TEST_SPRING_CLOUD_STREAM_PASSWORD', "huron-admin")
                     }
                 }
                 if (ciPlanName.equals("spring-cloud-stream-k8s-acceptance-tests")) {
