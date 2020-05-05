951b138a446e106b6bba0c2f1c9b845b8390d8a3
Soby Chacko <schacko@pivotal.io>
2020-05-05 18:02:38 -0400

    Update the CF AT environment info in the build

--- src/main/groovy/org/springframework/jenkins/springcloudstream/ci/SpringCloudStreamBuildMarker.groovy
+++ src/main/groovy/org/springframework/jenkins/springcloudstream/ci/SpringCloudStreamBuildMarker.groovy
@@ -80,7 +80,7 @@ class SpringCloudStreamBuildMarker implements JdkConfig, TestPublisher,
                 }
                 if (ciPlanName.equals("spring-cloud-stream-cf-acceptance-tests")) {
                     credentialsBinding {
-                        usernamePassword('CF_E2E_TEST_SPRING_CLOUD_STREAM_USER', 'CF_E2E_TEST_SPRING_CLOUD_STREAM_PASSWORD', "violet-admin")
+                        usernamePassword('CF_E2E_TEST_SPRING_CLOUD_STREAM_USER', 'CF_E2E_TEST_SPRING_CLOUD_STREAM_PASSWORD', "new-navy-PCF-Admin")
                     }
                 }
                 if (ciPlanName.equals("spring-cloud-stream-k8s-acceptance-tests")) {
