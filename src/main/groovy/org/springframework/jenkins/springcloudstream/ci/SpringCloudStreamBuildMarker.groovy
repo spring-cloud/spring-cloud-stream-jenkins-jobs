f4c9418f01534f4d99e3b3c6c798916a072576aa
Soby Chacko <schacko@pivotal.io>
2021-03-19 13:13:20 -0400

    snapshot docs fix: attempt - 1

--- src/main/groovy/org/springframework/jenkins/springcloudstream/ci/SpringCloudStreamBuildMarker.groovy
+++ src/main/groovy/org/springframework/jenkins/springcloudstream/ci/SpringCloudStreamBuildMarker.groovy
@@ -131,20 +131,20 @@ class SpringCloudStreamBuildMarker implements JdkConfig, TestPublisher,
                 }
             }
             configure {
-                if (docsBuild && (project.equals("spring-cloud-stream-binder-kafka") || project.equals("spring-cloud-stream-binder-rabbit")
+                if (branchToBuild.equals("master") && docsBuild && (project.equals("spring-cloud-stream-binder-kafka") || project.equals("spring-cloud-stream-binder-rabbit")
                                  || project.equals("spring-cloud-stream")) ) {
-                    //artifactoryMavenBuild(it as Node) {
-                        //mavenVersion(maven35())
-                        //if (project.equals("spring-cloud-stream-binder-kafka")) {
-                          //goals('clean install -U -Pdocs -Pspring -pl :spring-cloud-stream-binder-kafka-docs')
-                        //}
-                        //else if (project.equals("spring-cloud-stream-binder-rabbit")) {
-                          //goals('clean install -U -Pdocs -Pspring -pl :spring-cloud-stream-binder-rabbit-docs')
-                        //}
-                        //else if (project.equals("spring-cloud-stream")) {
-                          //goals('clean install -U -Pdocs -Pspring -pl :spring-cloud-stream-docs')
-                        //}
-                    //}
+                    artifactoryMavenBuild(it as Node) {
+                        mavenVersion(maven35())
+                        if (project.equals("spring-cloud-stream-binder-kafka")) {
+                          goals('clean install -U -Pdocs -Pspring -pl :spring-cloud-stream-binder-kafka-docs')
+                        }
+                        else if (project.equals("spring-cloud-stream-binder-rabbit")) {
+                          goals('clean install -U -Pdocs -Pspring -pl :spring-cloud-stream-binder-rabbit-docs')
+                        }
+                        else if (project.equals("spring-cloud-stream")) {
+                          goals('clean install -U -Pdocs -Pspring -pl :spring-cloud-stream-docs')
+                        }
+                    }
                     artifactoryMaven3Configurator(it as Node) {
                         if (isRelease && releaseType != null && releaseType.equals("milestone")) {
                             deployReleaseRepository("libs-milestone-local")
