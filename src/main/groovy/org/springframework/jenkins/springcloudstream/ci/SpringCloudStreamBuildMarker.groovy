19113cfe00b7cff62b7cdd44091ce28d80b449ea
Soby Chacko <chackos@vmware.com>
2021-07-20 17:02:53 -0400

    Update SpringCloudStreamBuildMarker.groovy

--- src/main/groovy/org/springframework/jenkins/springcloudstream/ci/SpringCloudStreamBuildMarker.groovy
+++ src/main/groovy/org/springframework/jenkins/springcloudstream/ci/SpringCloudStreamBuildMarker.groovy
@@ -80,7 +80,7 @@ class SpringCloudStreamBuildMarker implements JdkConfig, TestPublisher,
                 }
                 if (ciPlanName.equals("spring-cloud-stream-cf-acceptance-tests")) {
                     credentialsBinding {
-                        usernamePassword('CF_E2E_TEST_SPRING_CLOUD_STREAM_USER', 'CF_E2E_TEST_SPRING_CLOUD_STREAM_PASSWORD', "lahabra-height-pcf-Admin")
+                        usernamePassword('CF_E2E_TEST_SPRING_CLOUD_STREAM_USER', 'CF_E2E_TEST_SPRING_CLOUD_STREAM_PASSWORD', "lahabra-heights-pcf-admin")
                     }
                 }
                 if (ciPlanName.equals("spring-cloud-stream-k8s-acceptance-tests")) {
